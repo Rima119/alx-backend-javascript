a29fbe939f059aebca4d0ec1d03fa8b28932b892
Rim El Filali <rimo1902@gmail.com>
2023-03-22 20:32:56 +0000

    fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,9 @@
-function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200, 
-                body: 'Success',
-            });
-        } else reject(new Error('The fake API is not working currently'));
+export default function getFullResponseFromAPI(success) {
+  if (success) {
+    return Promise.resolve({
+      status: 200,
+      body: 'Success',
     });
   }
-  export default getFullResponseFromAPI;
+  return Promise.reject(new Error('The fake API is not working currently'));
+}
