e168448dd308b81b6e221c67fd9232b9eb12a302
Rim El Filali <rimo1902@gmail.com>
2023-03-24 00:48:29 +0000

    update

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -23,7 +23,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
   }
 
   static convertPrice(amount, conversionRate) {
