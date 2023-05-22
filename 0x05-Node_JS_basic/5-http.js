82e42663d8e0f94c371b6a616090f86e0f673dae
Rim El Filali <rimo1902@gmail.com>
2023-05-22 16:36:09 +0000

    add solution to task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,28 +1,20 @@
 const http = require('http');
-const databaQuery = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
-const filename = process.argv[2];
-const app = http.createServer();
-app.on('request', (request, response) => {
-  if (request.method === 'GET' && request.url === '/students') {
-    databaQuery(filename).then(({ httpResponse }) => {
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end(`This is the list of our students\n${httpResponse.join('\n')}`);
-    }).catch((e) => {
-      response.write('This is the list of our students\n');
-      response.end(e.message);
-    });
-  } else {
-    const body = 'Hello Holberton School!';
-    response
-      .writeHead(200, {
-        'Content-Type': 'text/plain',
-      });
-    response.write(body);
-    response.end();
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.end();
 });
-
 app.listen(1245);
-
 module.exports = app;
